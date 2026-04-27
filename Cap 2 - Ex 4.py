966729fa050bf4f6072f3ce225aa0eab80c1a7a2
igorfmarcelino <igorfmarcelino@gmail.com>
2026-04-27 20:08:34 -0300

    Primeira alteração

---
+++ Cap 2 - Ex 4.py
@@ -0,0 +1,10 @@
+Largura = 17
+Altura = 12.0  
+
+print("Largura//2 - Valor da expressão: "+str(Largura//2)+" / Tipo: "+ str(type(Largura//2))) #Minha resposta: Valor da expressão: 8 / Tipo: <class 'int'>
+
+print("Largura/2.0 - Valor da expressão: "+str(Largura/2.0)+" / Tipo: "+ str(type(Largura/2.0))) #Minha resposta: Valor da expressão: 8.5 / Tipo: <class 'float'>
+
+print("Altura/3 - Valor da expressão: "+str(Altura/3)+" / Tipo: "+ str(type(Altura/3))) #Minha resposta: Valor da expressão: 4.0 / Tipo: <class 'float'>
+
+print("1 + 2 * 5 - Valor da expressão: "+str(1 + 2 * 5)+" / Tipo: "+ str(type(1+2*5))) #Minha resposta: Valor da expressão: 11 / Tipo: <class 'int'>
