8d18f6e382f42aa11dc273b701464919fc1cacd2
igorfmarcelino <igorfmarcelino@gmail.com>
2026-04-28 18:25:12 -0300

    Primeiro commit

---
+++ Cap 3 - Ex 2.py
@@ -0,0 +1,10 @@
+print("Este programa calcula o valor a ser pago por um serviço. \n \nO usuário deverá fornecer a quantidade de horas trabalhadas e o valor da taxa por hora. \n \nVamos lá?")
+try:
+    horas = float(input("\nDigite as horas: "))
+    taxa = float(input("Digite a taxa: "))
+    if horas <= 40:
+        print("Valor a ser pago: "+str(round(horas*taxa, 2)))
+    else:
+        print("Valor a ser pago: "+str(round((40*taxa)+((horas-40)*taxa*1.5), 2)))
+except:
+    print("Erro, por favor utilize uma entrada numérica")
